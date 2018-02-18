d6c97455410e7a8600aeb32dfc1fb8659faa93bb
tcmal <>
2018-02-18 14:15:29 +0000

    Spotify support

--- .bin/bar.sh
+++ .bin/bar.sh
@@ -25,11 +25,13 @@ ensure_length ()
 # functions
 song() {
 	playing="$(mpc status | grep -o 'playing' )"
+	spotify="$(~/.bin/sp current | grep -o 'Title')"
 
 	if [ "$playing" == "playing" ]; then
 		echo "$p$(mpc current --format %title%)$p"
-	else
-		:
+	elif [ "$spotify" == "Title" ]; then
+		eval $(~/.bin/sp eval)
+		echo "$p$SPOTIFY_TITLE$p"
 	fi
 }
 
