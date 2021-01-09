e336ada0fa8670838e7588f7c4e42c7d3cba54a9
tcmal <oscar.shrimpton.personal@gmail.com>
2021-01-09 21:32:40 +0000

    Fix GTK Theme

--- programs/gtk-base16.nix
+++ programs/gtk-base16.nix
@@ -1,18 +1,68 @@
-# Set GTK's colour scheme to the base16 one.
+# Materia theme with custom base16 palette
+# Modified from https://gitlab.com/rycee/nur-expressions
 
-{ pkgs, colours, ... }:
+{ colours, pkgs, lib, ... }:
 
-derivation {
-    name = "gtk-base16";
+pkgs.stdenv.mkDerivation {
+    pname = "materia-theme";
+    version = "2020-09-28";
 
-    builder = "${pkgs.bash}/bin/bash";
-    args = with pkgs; [ "-c" ''
-        ${coreutils}/bin/mkdir -p $out;
-        HOME=$TMP ${base16-builder}/bin/base16-builder -t gtk-3.0 -s $customScheme -b ${colours.mode} > $out/gtk3.css
-        HOME=$TMP ${base16-builder}/bin/base16-builder -t gtk-2.0 -s $customScheme -b ${colours.mode} > $out/gtkrc-2.0
-    ''];
+    src = pkgs.fetchFromGitHub {
+        owner = "nana-4";
+        repo = "materia-theme";
+        rev = "e329aaee160c82e85fe91a6467c666c7f9f2a7df";
+        sha256 = "1qmq5ycfpzv0rcp5aav4amlglkqy02477i4bdi7lgpbn0agvms6c";
+    };
 
-    customScheme = (import ./base16-scheme-yml.nix) { inherit pkgs colours; };
+    nativeBuildInputs = with pkgs; [
+        bc
+        inkscape
+        optipng
+        sassc
+    ];
 
-    system = builtins.currentSystem;
+    dontConfigure = true;
+
+    # Fixes problem "Fontconfig error: Cannot load default config file"
+    FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [ pkgs.cantarell-fonts ]; };
+
+    theme = lib.generators.toKeyValue { } {
+        # Color selection copied from
+        # https://github.com/pinpox/nixos-home/blob/1cefe28c72930a0aed41c20d254ad4d193a3fa37/gtk.nix#L11
+        ACCENT_BG = (builtins.substring 1 6 colours.base0B);
+        ACCENT_FG = (builtins.substring 1 6 colours.base00);
+        BG = (builtins.substring 1 6 colours.base00);
+        BTN_BG = (builtins.substring 1 6 colours.base02);
+        BTN_FG = (builtins.substring 1 6 colours.base06);
+        FG = (builtins.substring 1 6 colours.base05);
+        HDR_BG = (builtins.substring 1 6 colours.base02);
+        HDR_BTN_BG = (builtins.substring 1 6 colours.base01);
+        HDR_BTN_FG = (builtins.substring 1 6 colours.base05);
+        HDR_FG = (builtins.substring 1 6 colours.base05);
+        MATERIA_SURFACE = (builtins.substring 1 6 colours.base02);
+        MATERIA_VIEW = (builtins.substring 1 6 colours.base01);
+        MENU_BG = (builtins.substring 1 6 colours.base02);
+        MENU_FG = (builtins.substring 1 6 colours.base06);
+        SEL_BG = (builtins.substring 1 6 colours.base0D);
+        SEL_FG = (builtins.substring 1 6 colours.base0E);
+        TXT_BG = (builtins.substring 1 6 colours.base02);
+        TXT_FG = (builtins.substring 1 6 colours.base06);
+        WM_BORDER_FOCUS = (builtins.substring 1 6 colours.base05);
+        WM_BORDER_UNFOCUS = (builtins.substring 1 6 colours.base03);
+
+        MATERIA_COLOR_VARIANT = "dark";
+        MATERIA_STYLE_COMPACT = "True";
+        UNITY_DEFAULT_LAUNCHER_STYLE = "False";
+    };
+
+    passAsFile = [ "theme" ];
+
+    postPatch = ''
+        patchShebangs .
+    '';
+
+    buildPhase = ''
+        export HOME="$NIX_BUILD_ROOT"
+        ./change_color.sh -t $out/share/themes -o Materia-Base16 "$themePath"
+    '';
 }
--- roles/programs.nix
+++ roles/programs.nix
@@ -4,7 +4,6 @@
 
 let decoratedConfig = {
         inherit config pkgs lib colours;
-        
     };
 in {
     # Use ZSH
@@ -27,10 +26,12 @@ in {
         home.packages = with pkgs; [
             # Utilities
             firefox-devedition-bin
+            thunderbird
             git
             htop
             kdeApplications.okular
             unzip
+            gnome3.dconf
 
             # Languages, Runtimes, etc.
             nodejs
@@ -63,9 +64,15 @@ in {
         programs.zsh = import ../programs/zsh.nix decoratedConfig;
 
         # GUI Applications
+        gtk = {
+            enable = true;
+            theme = {
+                name = "Materia-Base16";
+                package = (import ../programs/gtk-base16.nix decoratedConfig);
+            };
+        };
+
         home.file.".config/obsidian/themes/nord-base16.css".source = "${import ../programs/obsidian-base16.nix decoratedConfig}/nord-base16.css";
-        home.file.".gtkrc-2.0".source = "${import ../programs/gtk-base16.nix decoratedConfig}/gtkrc-2.0";
-        home.file.".config/gtk-3.0/gtk.css".source = "${import ../programs/gtk-base16.nix decoratedConfig}/gtk3.css";
         home.file.".vscode/extensions/base16-system".source = "${import ../programs/vscode-base16.nix decoratedConfig}/base16-system";
         home.file.".mozilla/localExtensions/base16-system.zip".source = "${import ../programs/firefox-base16.nix decoratedConfig}/addon.zip";
         home.file.".config/userstyles".source = "${import ../programs/userstyles.nix decoratedConfig}";
