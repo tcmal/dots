2567024f9e029fe4961388e918f7a040a12e513d
tcmal <oscar.shrimpton.personal@gmail.com>
2021-01-28 13:08:51 +0000

    change colour scheme

--- machines/vega.nix
+++ machines/vega.nix
@@ -10,24 +10,23 @@ let home-manager = builtins.fetchGit {
         menu = "${pkgs.rofi}/bin/rofi -show drun";
         colours = {
             mode = "dark";
-
-            base00 = "#1d1f21";
-            base01 = "#282a2e";
-            base02 = "#373b41";
-            base03 = "#969896";
-            base04 = "#b4b7b4";
-            base05 = "#c5c8c6";
-            base06 = "#e0e0e0";
-            base07 = "#ffffff";
-            base08 = "#CC342B";
-            base09 = "#F96A38";
-            base0A = "#FBA922";
-            base0B = "#198844";
-            base0C = "#3971ED";
-            base0D = "#3971ED";
-            base0E = "#A36AC7";
-            base0F = "#3971ED";
-
+            
+            base00 = "#212121";
+            base01 = "#303030";
+            base02 = "#353535";
+            base03 = "#4A4A4A";
+            base04 = "#B2CCD6";
+            base05 = "#EEFFFF";
+            base06 = "#EEFFFF";
+            base07 = "#FFFFFF";
+            base08 = "#89DDFF";
+            base09 = "#F78C6C";
+            base0A = "#FFCB6B";
+            base0B = "#C3E88D";
+            base0C = "#89DDFF";
+            base0D = "#82AAFF";
+            base0E = "#C792EA";
+            base0F = "#FF5370";
         };
     };
 in {
