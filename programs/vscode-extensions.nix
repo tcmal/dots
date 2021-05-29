35831b17198d830b6debf8891e535085072b0825
tcmal <oscar.shrimpton.personal@gmail.com>
2021-05-29 15:35:56 +0100

    vscode: beautify & preview-pdf

--- programs/vscode-extensions.nix
+++ programs/vscode-extensions.nix
@@ -31,6 +31,18 @@ let
             version = "5.4.0";
             sha256 = "c90c0e9542b65e27dcd32d4722fc8ab2cef7382c696412a74ea32b3604f9605a";
         }
+        {
+            name = "preview-pdf";
+            publisher = "analytic-signal";
+            version = "0.0.3";
+            sha256 = "c83f26bef3b0714c97d8b2f3f8dc76d8f2a790f14defe076c79cbb037f7fb800";
+        }
+        {
+            name = "beautify";
+            publisher = "HookyQR";
+            version = "1.5.0";
+            sha256 = "b1480742dbc8369aa10f348d7d27c589e6d9bd7d00e62e5255903fdeb67213b0";
+        }
     ];
 in pkgs.vscode-with-extensions.override {
     vscodeExtensions = extensions;
