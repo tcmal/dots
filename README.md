bc97a12490f4ab99e40e4bbc1e0748969bb14570
tcmal <oscar.shrimpton.personal@gmail.com>
2022-03-30 15:26:24 +0100

    move to fedora

--- README.md
+++ README.md
@@ -1,32 +1,7 @@
 # dots
 
-Dotfiles in NixOS configuration format.
+Dotfiles. I don't use NixOS anymore, so these use `stow`, and `org-babel-tangle`.
 
-Machines are specific hosts that i use.
-Roles are sets of programs/configs that i want to run in combination.
-Programs are program-specific configuration, usually using home-manager.
-
-# colours
-
-I use base16 to sync the same colour scheme across the following applications:
-  - `alacritty`
-  - `discord`
-  - `firefox`
-  - `gtk2/3`
-  - `i3`
-  - `rofi`
-  - `spotify`
-  - `vscode`
-
-This can be seen in `roles/desktop.nix`
-
-# license
-
-Copyright belongs to me and is under the MIT License, except for:
-  - `share/obsidian.css` - ['Nord' obsidian theme. Unknown License](https://github.com/insanum/obsidian_nord)
-  - `share/spotify.css` - ['Nord' spicetify theme. MIT License](https://github.com/morpheusthewhite/spicetify-themes)
-  - `programs/discord-base16.nix` - ['Comfy' discord theme. GNU-GPLv3](https://github.com/NYRI4/Comfy)
-  - `programs/gtk-base16.nix` - [Materia customising package. MIT License](https://gitlab.com/rycee/nur-expressions)
-  - `share/shutdown-menu` - [Script by Benjamin Chrétien. GNU-GPLv3 License](https://github.com/vivien/i3blocks-contrib/)
-  - `share/wallpaper.png` - [Celeste, Chapter 7. Developed by Matt Makes Games](https://store.steampowered.com/app/504230/Celeste/)
-  - `share/userstyles/google.ejs` - [Google Dark Rainbow. MIT License](https://github.com/exmert/Google-Dark---Rainbow)
+```
+$ stow -t ~ xmonad
+```
---
+++ xmonad/.xmonad/xmonad.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE RebindableSyntax #-}
+
+import XMonad.Config.Prime
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+import XMonad.Layout.Spacing
+import qualified XMonad.Layout.Fullscreen as F
+
+import XMonad.Actions.CycleWS (toggleWS)
+
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.EwmhDesktops as E
+import XMonad.Config.Xfce
+
+-- force fullscreen bind
+toggleFull = withFocused (\windowId -> do
+    { floats <- gets (W.floating . windowset);
+        if windowId `M.member` floats
+        then withFocused $ windows . W.sink
+        else withFocused $ windows . (flip W.float $ W.RationalRect 0 0 1 1) })  
+
+main :: IO ()
+main = xmonad $ do
+    startWith xfceConfig
+
+    -- looks
+    logHook =+ fadeInactiveLogHook 0.9
+    borderWidth =: 0
+    modifyLayout $ spacing 10
+
+    -- fullscreen button
+    handleEventHook =+ F.fullscreenEventHook
+    handleEventHook =+ E.fullscreenEventHook
+    manageHook =+ F.fullscreenManageHook
+    modifyLayout $ F.fullscreenFloat
+
+    -- applications
+    terminal =: "xfce4-terminal"
+    
+    -- keybinds
+    modMask =: mod4Mask
+    keys =- ["M-S-q", "M-P", "M-S-P", "M-S-C", "M-S-/", "M-<Tab>", "M-S-<Tab>", "M-b"]
+    withWorkspaces (do
+        wsKeys =+ ["0"])
+
+    keys =+ [
+                ("M-<Return>", spawn "xfce4-appfinder"),
+                ("M-t", spawn "xfce4-terminal"),
+                ("M-S-r", spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"),
+
+                -- -- shutdown menu
+                ("M-g", spawn "xfce4-session-logout"),
+
+                -- window management
+                ("M-S-q", kill),
+                ("M-<Tab>", toggleWS),
+                ("M-f", windows W.swapMaster),
+                ("M-,", sendMessage (IncMasterN (-1))), ("M-.", sendMessage (IncMasterN 1)),
+                ("M-S-f", toggleFull)
+            ]
